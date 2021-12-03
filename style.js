4df9c0220086ca5dad7f8d7a034a1c5213999ab4
tinnguyen14 <tinshb@gmail.com>
2021-12-03 19:25:33 +0700

    first commit

---
+++ style.js
@@ -0,0 +1,21 @@
+const $=document.querySelector.bind(document)
+var barmobie=$('.bar-mob');
+var menu=$('.menu-bottom');
+var blackbg=$('.black-bg');
+var close=$('.close');
+
+barmobie.onclick=() =>{
+    menu.classList.add('active')
+    blackbg.classList.add('active')
+    close.classList.add('active')
+}
+blackbg.onclick=() =>{
+    menu.classList.remove('active')
+    blackbg.classList.remove('active')
+    close.classList.remove('active')
+}
+close.onclick=() =>{
+    menu.classList.remove('active')
+    blackbg.classList.remove('active')
+    close.classList.remove('active')
+}
