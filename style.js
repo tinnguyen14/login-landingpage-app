dbfb561ed3f9b0d4f6b9a001f6331a5da8907076
tinnguyen14 <tinshb@gmail.com>
2021-12-05 18:31:58 +0700

    next commit

--- style.js
+++ style.js
@@ -3,21 +3,25 @@ var barmobie=$('.bar-mob');
 var menu=$('.menu-bottom');
 var blackbg=$('.black-bg');
 var close=$('.close');
+var body=$('body')
 
 barmobie.onclick=() =>{
     menu.classList.add('active')
     blackbg.classList.add('active')
     close.classList.add('active')
+    body.classList.add('no-scroll')
 }
 blackbg.onclick=() =>{
     menu.classList.remove('active')
     blackbg.classList.remove('active')
     close.classList.remove('active')
+    body.classList.remove('no-scroll')
 }
 close.onclick=() =>{
     menu.classList.remove('active')
     blackbg.classList.remove('active')
     close.classList.remove('active')
+    body.classList.remove('no-scroll')
 }
 window.onscroll=() =>{
     if(window.pageYOffset>40){
